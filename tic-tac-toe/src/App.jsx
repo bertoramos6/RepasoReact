20c8eea9e6f1b6f7b2a366234d9447343f9b0b68
Alberto Ramos Suárez <bertoramos2001@gmail.com>
2024-05-28 13:15:31 +0200

    cambios

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -7,14 +7,26 @@ import { checkWinner, checkEndGame } from "./logic/board"
 import { WinnerModal } from "./components/WinnerModal"
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  // A los useState, en lugar de pasarles el estado inicial, se les puede pasar una
+  // función que condicionalmente haga return del estado que ha de ser inicial (porque
+  // recordemos que no podemos meter el useState en un if)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null) //null es que no hay ganador, false es que hay empate
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
   
   const updateBoard = (index) => {
@@ -27,6 +39,9 @@ function App() {
     // Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // Guardar cambios en localStorage
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
     // Revisar si hay ganador
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
